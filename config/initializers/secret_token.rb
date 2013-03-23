aa69f88cf40533df234318b7bda88fd648d160b4
Avi-ec2-kyp <ubuntu@ip-10-98-171-12.ec2.internal>
2013-03-23 17:03:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NextWho::Application.config.secret_token = '8eb78cf75701b71a7e035db9cda931cb3fdd02d806311fc3873209e384ff976a63db6206b7b9b9e86beff19be52bb54d17e7e2046f851a95a66d876a02c331b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NextWho::Application.config.session_store :cookie_store, key: '_nextWho_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NextWho::Application.config.session_store :active_record_store
